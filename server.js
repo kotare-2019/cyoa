96ae516de849e0023f021480e2ea64b0a8b34260
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-06-06 15:50:06 +1200

    Pulled pupparazi skeleton

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const server = express()
+
+// Middleware
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({extended: false}))
+
+module.exports = server
