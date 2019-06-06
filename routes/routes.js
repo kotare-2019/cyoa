a61262240b5ef484c44b11eb519eafcc252a17cb
Reuben Osborne <10952926+reubenosborne@users.noreply.github.com>
2019-06-06 19:16:25 +1200

    Add new scenes and new routes

--- routes/routes.js
+++ routes/routes.js
@@ -15,8 +15,8 @@ router.get("/begin/", (req, res) => {
 
     const items = data.character["items"]
 
-    if(items.length == 0) {
-        items.push("A rusty key","A small shovel","A glowing crystal")
+    if (items.length == 0) {
+        items.push("A rusty key", "A small shovel", "A glowing crystal")
     }
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8", err => {
@@ -29,7 +29,7 @@ router.get("/begin/", (req, res) => {
 
 router.get("/forest/", (req, res) => {
 
-    if(!data.path.includes("forest")){
+    if (!data.path.includes("forest")) {
         data.path.push("forest")
     }
 
@@ -43,7 +43,7 @@ router.get("/forest/", (req, res) => {
 
 router.get("/swamp/", (req, res) => {
 
-    if(!data.path.includes("swamp")){
+    if (!data.path.includes("swamp")) {
         data.path.push("swamp")
     }
 
@@ -54,13 +54,48 @@ router.get("/swamp/", (req, res) => {
     res.render("scenes/swamp", data)
 });
 
-router.get("/scene3/", (req, res) => {
-    res.render("scenes/scene3", data)
+router.get("/castle/", (req, res) => {
+
+
+    if (!data.path.includes("castle")) {
+        data.path.push("castle")
+    }
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8", err => {
+        if (err) throw err;
+    });
+
+    res.render("scenes/castle", data)
 });
 
-router.get("/scene4/", (req, res) => {
-    res.render("scenes/scene4", data)
+router.get("/dungeon/", (req, res) => {
+
+    if (!data.path.includes("dungeon")) {
+        data.path.push("dungeon")
+    }
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8", err => {
+        if (err) throw err;
+    });
+
+    res.render("scenes/dungeon", data)
 });
+
+router.post("/home", (req, res) => {
+
+    if (!data.character.name) {
+        data.character.name = req.body.name
+        data.character.fear = req.body.fear
+        data.character.love = req.body.love
+    }
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), "utf8", err => {
+        if (err) throw err;
+    });
+
+    res.redirect("/begin/")
+
+})
 
 
 module.exports = router;
